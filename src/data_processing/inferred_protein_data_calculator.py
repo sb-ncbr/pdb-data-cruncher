3b14ccde5780de612265880125a8a2aaef2ff627
Iveta Strnadová <ivka.strnadova@gmail.com>
2024-02-14 17:18:15 +0100

    fix: handle 0 values in harmonic mean better

--- src/data_processing/inferred_protein_data_calculator.py
+++ src/data_processing/inferred_protein_data_calculator.py
@@ -1,4 +1,6 @@
-from src.models import ProteinDataComplete, ProteinDataInferred, Diagnostics
+from statistics import harmonic_mean
+
+from src.models import ProteinDataComplete, ProteinDataInferred
 
 
 def calculate_inferred_protein_data(data: ProteinDataComplete) -> None:
@@ -14,22 +16,25 @@ def calculate_inferred_protein_data(data: ProteinDataComplete) -> None:
     sidechain_perc = data.xml.absolute_percentile_percent_rota_outliers
     rna_perc = data.xml.absolute_percentile_rna_suiteness
 
-    if clashscore_perc and rama_perc and sidechain_perc:  # if all are not None and not 0
-        summation = 1.0 / clashscore_perc + 1.0 / rama_perc + 1.0 / sidechain_perc
+    if clashscore_perc is not None and rama_perc is not None and sidechain_perc is not None:
+        values_for_harmonic_mean = [clashscore_perc, rama_perc, sidechain_perc]
         if rna_perc is not None:
-            summation += 1.0 / rna_perc
-        data.inferred.combined_geometry_quality = 1.0 / (summation / 3.0)
+            values_for_harmonic_mean.append(rna_perc)
+        data.inferred.combined_geometry_quality = harmonic_mean(values_for_harmonic_mean)
 
     r_free_perc = data.xml.absolute_percentile_dcc_r_free
     rsrz_perc = data.xml.absolute_percentile_percent_rsrz_outliers
 
-    if r_free_perc and rsrz_perc:  # if all are not None and not 0
-        summation = 1.0 / r_free_perc + 1.0 / rsrz_perc
-        data.inferred.combined_x_ray_quality_metric = 1.0 / (summation / 2.0)
+    if r_free_perc is not None and rsrz_perc is not None:
+        data.inferred.combined_x_ray_quality_metric = harmonic_mean([r_free_perc, rsrz_perc])
 
-    if data.inferred.combined_x_ray_quality_metric is not None and data.inferred.combined_geometry_quality is not None and data.pdbx.resolution is not None:
+    if (
+        data.inferred.combined_x_ray_quality_metric is not None
+        and data.inferred.combined_geometry_quality is not None
+        and data.pdbx.resolution is not None
+    ):
         data.inferred.combined_overall_quality_metric = (
-            data.inferred.combined_x_ray_quality_metric +
-            data.inferred.combined_geometry_quality -
-            30.0 * data.pdbx.resolution
+            data.inferred.combined_x_ray_quality_metric
+            + data.inferred.combined_geometry_quality
+            - 30.0 * data.pdbx.resolution
         ) / 2
