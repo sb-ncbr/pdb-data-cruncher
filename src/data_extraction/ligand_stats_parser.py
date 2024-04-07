e9b31a7bda7316b8a83159001d9e2bb9a353d9a4
Iveta Strnadová <ivka.strnadova@gmail.com>
2024-04-07 15:28:20 +0200

    fix: ligand parser works for all

--- src/data_extraction/ligand_stats_parser.py
+++ src/data_extraction/ligand_stats_parser.py
@@ -4,7 +4,7 @@ import os.path
 
 from pdbeccdutils.core import ccd_reader
 from rdkit import Chem
-from rdkit.Chem import rdMolDescriptors, Lipinski
+from rdkit.Chem import Lipinski
 
 from src.models import LigandInfo
 from src.exception import ParsingError
@@ -94,26 +94,33 @@ def calculate_ligand_stats(
 
 
 def _calculate_one_ligand_stats(path_to_ccd_files: str, ligand_id: str) -> LigandInfo:
+    """
+    Calculate stats for one ligand.
+    :param path_to_ccd_files:
+    :param ligand_id:
+    :return: Ligand info instance loaded with ligand stats.
+    """
     ligand_cif_path = os.path.join(path_to_ccd_files, f"{ligand_id}.cif")
     ligand_full_mol = ccd_reader.read_pdb_cif_file(ligand_cif_path).component.mol
 
-    # TODO try to remove the try and just calculate it all - after comprehensive test
-    try:
-        ligand_mol_without_hs = Chem.RemoveHs(ligand_full_mol)
-        rotatable_bond_count = rdMolDescriptors.CalcNumRotatableBonds(ligand_mol_without_hs, strict=False)
-    except Chem.rdchem.AtomValenceException:
-        rotatable_bond_count = _calculate_rotatable_bonds_for_faulty_atom_valence(ligand_full_mol)
-
+    rotatable_bond_count = _calculate_rotatable_bonds(ligand_full_mol)
     total_bonds = ligand_full_mol.GetNumBonds()
 
     return LigandInfo(
         id=ligand_id,
         heavy_atom_count=Lipinski.HeavyAtomCount(ligand_full_mol),
-        flexibility=(rotatable_bond_count / total_bonds if total_bonds > 0 else 0),
+        flexibility=(rotatable_bond_count / total_bonds if total_bonds > 0 else 1),
     )
 
 
-def _calculate_rotatable_bonds_for_faulty_atom_valence(ligand_full_mol: Chem.Mol) -> int:
+def _calculate_rotatable_bonds(ligand_full_mol: Chem.Mol) -> int:
+    """
+    Calculate number of rotatable bonds, eg. those not of double/triple bond type, not terminal and not part of cycle.
+    :param ligand_full_mol: Mol representation of the ligand, full (with Hs).
+    :return: Number of rotatable bonds.
+    """
+    # sanitization is off because the cif is loaded into the mol without adjusting charge of atoms, which would result
+    # in AtomValenceException with sanitization on
     mol_withouth_hs: Chem.Mol = Chem.RemoveHs(ligand_full_mol, sanitize=False)
     bond_count = 0
 
@@ -123,9 +130,39 @@ def _calculate_rotatable_bonds_for_faulty_atom_valence(ligand_full_mol: Chem.Mol
             continue  # double or triple bond cannot rotate
         if bond.GetBeginAtom().GetDegree() == 1 or bond.GetEndAtom().GetDegree() == 1:
             continue  # one of the atoms is terminal
-        if bond.IsInRing():
+        if _bond_is_in_ring(bond):
             continue
 
         bond_count += 1
 
     return bond_count
+
+
+def _bond_is_in_ring(bond_to_test: Chem.Bond) -> bool:
+    """
+    Check if the bond is part of the ring - if other "path" exists between its atoms other than this bond.
+    :param bond_to_test:
+    :return: True if bond is part of the ring.
+    """
+    end_atom_id = bond_to_test.GetEndAtomIdx()
+    current_atom = bond_to_test.GetBeginAtom()
+    explored_atom_ids = [current_atom.GetIdx()]
+    atoms_to_explore = []
+
+    for neighbor in current_atom.GetNeighbors():
+        if neighbor.GetIdx() != end_atom_id:  # try to find path other than this bond's one
+            atoms_to_explore.append(neighbor)
+
+    while len(atoms_to_explore) > 0:
+        current_atom = atoms_to_explore.pop(0)
+        current_atom_id = current_atom.GetIdx()
+        explored_atom_ids.append(current_atom_id)
+
+        for neighbor in current_atom.GetNeighbors():
+            neighbor_id = neighbor.GetIdx()
+            if neighbor_id == end_atom_id:
+                return True  # found another way to end atom -> bond is part of cycle
+            if neighbor_id not in explored_atom_ids:
+                atoms_to_explore.append(neighbor)
+
+    return False
