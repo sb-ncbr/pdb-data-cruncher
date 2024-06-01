b0b52647e8343d2544e5ec11dbf047143d5fda08
Iveta Strnadová <ivka.strnadova@gmail.com>
2024-06-01 22:09:33 +0200

    feat: change source for zipping and change compression level

--- src/data_archivation/data_archivation_manager.py
+++ src/data_archivation/data_archivation_manager.py
@@ -1,6 +1,7 @@
 import logging
 import subprocess
 from os import path
+from typing import Optional
 
 from src.config import Config
 from src.exception import DataArchivationError
@@ -13,16 +14,23 @@ class DataArchivationManger:
     """
 
     @staticmethod
-    def update_archive(archive_filepath: str, folder_to_archive_path: str) -> None:
+    def update_archive(
+        archive_filepath: str, folder_to_archive_path: str, max_process_count: int, compression_level: Optional[str]
+    ) -> None:
         """
         Updates the archive via 7z commandline utility.
         :param archive_filepath:
         :param folder_to_archive_path:
+        :param max_process_count:
+        :param compression_level:
         """
         logging.info("Starting to update archive %s with data from %s.", archive_filepath, folder_to_archive_path)
         # u => update the archive
         # -uq0 => defines behaviour for files that exist in the archive but not in data to archive as: delete in archive
-        command = ["7z", "u", archive_filepath, "-uq0", folder_to_archive_path]
+        command = ["7z", "u", archive_filepath]
+        if compression_level:
+            command.append(f"-mx={compression_level}")
+        command.extend([f"-mmt{max_process_count}", "-uq0", folder_to_archive_path])
         logging.debug("Running command: %s", " ".join(command))
         try:
             subprocess.run(command, check=True, capture_output=True)
@@ -42,18 +50,20 @@ def run_data_archivation(config: Config) -> bool:
     """
     logging.info("PHASE DATA ARCHIVATION is starting")
     data_to_archive = [  # source folder and resulting archive name
-        (config.filepaths.pdb_mmcifs, "rawpdbe.7z"),
-        (config.filepaths.xml_reports, "rawvalidxml.7z"),
-        (config.filepaths.rest_jsons, "rawrest.7z"),
-        (config.filepaths.validator_db_results, "rawvdb.7z"),
-        (config.filepaths.ligand_cifs, "rawccd.7z")
+        (config.filepaths.gz_pdb_mmcifs, "rawpdbe.7z", 0),
+        (config.filepaths.gz_xml_reports, "rawvalidxml.7z", 0),
+        (config.filepaths.rest_jsons, "rawrest.7z", None),
+        (config.filepaths.validator_db_results, "rawvdb.7z", None),
+        (config.filepaths.ligand_cifs, "rawccd.7z", None)
     ]
     failed_count = 0
 
-    for data_source_folder, final_archive_name in data_to_archive:
+    for data_source_folder, final_archive_name, compression_level in data_to_archive:
         archive_path = path.join(config.filepaths.output_root_path, final_archive_name)
         try:
-            DataArchivationManger.update_archive(archive_path, data_source_folder)
+            DataArchivationManger.update_archive(
+                archive_path, data_source_folder, config.max_process_count, compression_level
+            )
         except DataArchivationError as ex:
             logging.error(ex)
             failed_count += 1
